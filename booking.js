07d7f183d0d51354217b8985561e2d916b56f65d
K1mmyn <125098192+K1mmyn@users.noreply.github.com>
2023-08-19 22:23:02 +1200

    Closest Dates for homepage & Comments

--- booking.js
+++ booking.js
@@ -1,4 +1,3 @@
-
 let currentAuthMode = "";
 let user = null;
 const authDiv = document.getElementById("booking-auth-div")
@@ -11,37 +10,41 @@ const changeAuthMode = (authModeID) => {
 const onLoginSubmit = (event) => {
 
    event.preventDefault();
+
+   // Getting user information
    const userEmail = document.getElementById("user-email").value
    const userPassword = document.getElementById("user-password").value
 
+   // Checking if all fields are filled
    if (!userEmail || !userPassword) {
       return alert("Please fill out all fields")
    }
 
+   // Checking for previous user data
    const allUsersInLocalStorage = JSON.parse(localStorage.getItem("user-db"))
 
+   // If there are no previous users they are not allowed to sign in
    if (!allUsersInLocalStorage) {
-      return alert("User not found")
+      return alert("No Users currently in Database")
    }
 
+   // Finding the user from localStorage
    const userToFind = allUsersInLocalStorage.find(user => user.userEmail === userEmail && user.userPassword === userPassword)
 
+   // If the given information doesn't match with any user in database 
    if (!userToFind) {
-      return alert("User not found")
+      return alert('Incorrect Password or Email')
    }
 
-   return alert("User found")
+   // If we have gotten to this point it means we have found a user in the data base
+   user = userToFind
+
+   // Welcoming user
+   alert(`Welcome back to Maunga Club ${userToFind.userFullName}`)
+
+   // Now that users are signed in, They are allowed to find booking dates
+   return renderCalendar()
 }
-// const userEmail = document.getElementById("user-email")
-// const userPassword = document.getElementById("user-password")
-
-// const newUser = {
-//    id: Math.random(),
-//    email: userEmail,
-//    password: userPassword,
-// }
-
-// localStorage.getItem("user-auth")
 
 const onRegisterSubmit = (event) => {
    event.preventDefault();
@@ -98,21 +101,35 @@ Book for your next adventure now!`)
    // Saving data to localStorage
    localStorage.setItem("user-db", JSON.stringify(prevData))
    user = newUser
+
+   // Alerting user of successfil registration
    alert(`Welcome to Maunga Club ${newUser.userFullName.charAt(0).toUpperCase() + newUser.userFullName.slice(1)} 
 Book for your next adventure now!`)
+
+   // Now that users are signed in, They are allowed to find booking dates
    return renderCalendar()
 }
 
 const onResetPassword = (event) => {
    event.preventDefault();
+
+   // Getting user email
    const inputEmail = document.getElementById("reset-password-input").value
-   if (!inputEmail) return alert("Please enter a valid email")
+
+   // Checking if the field is filled out
+   if (!inputEmail) return alert("Please fill out all fields")
+
+   // Alerting user of the email is has been sent to
    alert(`Reset Password Email has been sent to ${inputEmail}`)
+
+   // Automatically sending user to login page for conveince 
    currentAuthMode = "login"
    renderAuth()
 }
 
 const renderAuth = () => {
+
+   // If there isn't an authMode that means the user has just opened the website so return a button for them to sign in
    if (!currentAuthMode) {
       authDiv.innerHTML = `
       <div class="login-container" style="width: 100vw; align-items: center;  justify-content: center; padding-bottom: 10vw;">
@@ -202,6 +219,13 @@ const renderAuth = () => {
    }
 }
 
+const scrollToID = (id) => {
+   document.getElementById(id)?.scrollIntoView({
+      behavior: "smooth",
+      block: "center",
+   });
+};
+
 
 // Container to append to
 const calendarContainer = document.getElementById("booking-auth-div")
@@ -217,12 +241,53 @@ let booking = ''
 let arrowClicks = 0
 let currentMonth = new Date().getMonth();
 let curentYear = new Date().getFullYear()
+let calendarAnimationDuration = 1000
+let calendar = ""
+
+const setDayHeader = (dayID) => {
+
+   // New date will give us a 0 for Sunday, 1 for Monday and so on
+   // This helps us convert those numbers into strings we can use for the calendar
+
+   let dayConverted;
+
+   switch (dayID) {
+      case 0:
+         dayConverted = "Sunday"
+         break;
+      case 1:
+         dayConverted = "Monday"
+         break
+      case 2:
+         dayConverted = "Tuesday"
+         break
+      case 3:
+         dayConverted = "Wednesday"
+         break
+      case 4:
+         dayConverted = "Thursday"
+         break
+      case 5:
+         dayConverted = "Friday"
+         break
+      case 6:
+         dayConverted = "Saturday"
+         break
+      default:
+         break;
+   }
+
+   return dayConverted
+
+}
 
 const dateAvailabilities = () => {
 
    booking = ""
 
    for (let i = 0; i < cabins.length; i++) {
+
+      // This if statement check to see if the day they have clicked on is not Sunday or Friday or Saturday meaning it is unavailable
       if (dayHeader !== "Sunday" && dayHeader !== "Friday" && dayHeader !== "Saturday") {
          return booking += `
             <div style="background-color: #EF4565; width:100%; animation: booking-slide-up 1s ease-in-out" class='div'>
@@ -231,11 +296,13 @@ const dateAvailabilities = () => {
          `
       }
 
+      // Simulating booked dates
+      // We can use a random number for this because if we click in and out of date there will be different results to its availablity 
       if ((day + i) % 3 > 0) {
          booking += `
             <div style="background-color: #7DB542; animation-duration: 1s;" class="div">
                ${cabins[i]}
-               <a href='booking.html' target='blank' onclick='checkPreviousDates()'> 
+               <a href='booking.html' target='blank'>
                   <div class="booking-hover-button">
                      BOOK !
                   </div>
@@ -251,46 +318,22 @@ const dateAvailabilities = () => {
    }
 }
 
-let calendarAnimationDuration = 1000
-let calendar = ""
-
-const setDayHeader = (dayID) => {
-   switch (dayID) {
-      case 0:
-         dayHeader = "Sunday"
-         break;
-      case 1:
-         dayHeader = "Monday"
-         break
-      case 2:
-         dayHeader = "Tuesday"
-         break
-      case 3:
-         dayHeader = "Wednesday"
-         break
-      case 4:
-         dayHeader = "Thursday"
-         break
-      case 5:
-         dayHeader = "Friday"
-         break
-      case 6:
-         dayHeader = "Saturday"
-         break
-      default:
-         break;
-   }
-}
-
 const handleCalendarDateClick = (dayID, dayParam) => {
+
+   // Warning user that if they click on a previous month, booking may not be available
    if (currentMonth < new Date().getMonth()) {
       alert("Booking for this date may not be available as it has already passed")
    }
+
+   // dayParam is just the date itselt example the 3rd in 3rd of August
    day = dayParam
+
+   // Resetting arrows clicks 
    arrowClicks = 0
-   setDayHeader(dayID)
+
+   // dayID is the day we have clicked 0 for Sunday etc
+   dayHeader = setDayHeader(dayID)
    renderCalendar()
-   checkPreviousDates()
 }
 
 const handleBetweenMonthsClick = (id, date, dayParam) => {
@@ -300,11 +343,11 @@ const handleBetweenMonthsClick = (id, date, dayParam) => {
    if (id === "next") {
       currentMonth += 1
       day = date
-      setDayHeader(dayParam)
+      dayHeader = setDayHeader(dayParam)
    } else if (id === "prev") {
       currentMonth -= 1
       day = date
-      setDayHeader(dayParam)
+      dayHeader = setDayHeader(dayParam)
    }
 
    renderCalendar()
@@ -312,13 +355,18 @@ const handleBetweenMonthsClick = (id, date, dayParam) => {
 
 const handleCalendarArrowClick = (id) => {
    if (id === "next") {
+      // To check that to see if we are on december
       if (currentMonth + 1 === 12) {
+         // If we are do nothing
          return
       }
       currentMonth += 1
       arrowClicks += 1
    } else if (id === "prev") {
+
+      // To check that to see if we are on december
       if (currentMonth - 1 === -1) {
+         // If we are do nothing
          return
       }
       currentMonth -= 1
@@ -346,6 +394,7 @@ const renderCalendar = () => {
    calendar = ""
    calendarAnimationDuration = 1000
 
+   // Calendar headers
    for (let i = 0; i < daysOfTheWeek.length; i++) {
 
       if (dayCounter === 7) {
@@ -360,6 +409,7 @@ const renderCalendar = () => {
       dayCounter += 1
    }
 
+   // Previous Dates
    for (let i = firstDayOfTheMonth; i > 0; i--) {
 
       if (dayCounter === 7) {
@@ -375,6 +425,7 @@ const renderCalendar = () => {
       dayCounter += 1
    }
 
+   // Dates for current month
    for (let i = 1; i <= lastDateOfTheMonth; i++) {
 
       if (dayCounter === 7) {
@@ -390,6 +441,7 @@ const renderCalendar = () => {
       dayCounter += 1
    }
 
+   // Following months date
    for (let i = lastDayOfTheMonth; i < 6; i++) {
 
       if (dayCounter === 7) {
@@ -406,7 +458,7 @@ const renderCalendar = () => {
    }
 
    calendarContainer.innerHTML = `
-      <div style="padding: 5vw; display: flex; justify-content: space-between; width: 100vw; height: 57.5vw; border: 1px solid black;">
+      <div style="padding: 5vw; display: flex; justify-content: space-between; width: 100vw; height: 57.5vw;">
          <div style="width: 27.5vw; height: 100%; background-color: white; border-radius: 2vw; display: flex; flex-direction: column; justify-content: space-around; padding: 2vw">
             <div style="width: 100%; height: 60%; display: flex; flex-direction: column;" id="calendar-container">
                <div style="width: 100%; display: flex; justify-content: space-between; font-size: 2vw; font-weight: 600; align-items: center; margin-bottom: 2vw; padding: 0vw 1vw;">
@@ -479,8 +531,78 @@ const renderCalendar = () => {
    `
 }
 
-if (!user) renderAuth();
-else {
-   renderCalendar();;
+
+const getClosestAvaliableDate = () => {
+
+   const currentMonth = new Date().getMonth()
+   const currentYear = new Date().getFullYear()
+   const currentDate = new Date().getDate()
+   const availableDateDiv = document.getElementById('available-dates')
+
+   let closestAvailableDatesArray = []
+   let closestAvailableDates = ''
+   let animationDuration = 800
+   let counter = 0
+   while (currentDate + counter < new Date(currentYear, currentMonth, 0).getDate() && closestAvailableDatesArray.length < 5) {
+      const date = new Date(currentYear, currentMonth, currentDate + counter)
+      if (date.getDay() === 0 && date.getDate() > currentDate || date.getDay() === 5 && date.getDate() > currentDate || date.getDay() === 6 && date.getDate() > currentDate) {
+         closestAvailableDatesArray.push(date)
+      }
+      counter += 1
+   }
+   counter = 0
+   if (closestAvailableDatesArray.length < 5) {
+
+      while (closestAvailableDatesArray.length < 5) {
+         const date = new Date(currentYear, currentMonth + 1, 0 + counter)
+         if (date.getDay() === 0 || date.getDay() === 5 || date.getDay() === 6) {
+            closestAvailableDatesArray.push(date)
+         }
+         counter += 1
+      }
+   }
+
+
+   for (let i = 0; i < closestAvailableDatesArray.length; i++) {
+      const closestDayHeader = setDayHeader(closestAvailableDatesArray[i].getDay())
+
+      if (i === 0 || closestAvailableDatesArray[i].getMonth() > currentMonth) {
+         closestAvailableDates += `
+            <div class="homepage-booking-container" style="animation-duration: ${animationDuration}ms;">
+               <div style="width: 7.5vw; position: absolute; font-size: 1vw;  text-align: center; top: -2vw; font-weight: 700; color: white; display: flex; justify-content: center;">
+                  <h3 style='text-transform: uppercase;'>
+                     ${monthsOfTheYear[closestAvailableDatesArray[i].getMonth()].substring(0, 3)}
+                  </h3>
+               </div>
+               <h3 style='text-transform: uppercase;'>${closestDayHeader.substring(0, 3)} ${closestAvailableDatesArray[i].getDate()}</h3>
+               <p >from</p>
+               <h3 >$250</h3>
+               <button class="homepage-booking-container-button" onclick="scrollToID('booking-auth-div')">BOOK</button>
+               </div>
+               `
+         animationDuration += 200
+      } else {
+         closestAvailableDates += `
+         <div class="homepage-booking-container" style="animation-duration: ${animationDuration}ms;">
+         <div style="width: 7.5vw; position: absolute; font-size: 1vw;  text-align: center; top: -2vw; font-weight: 700; color: white; display: flex; justify-content: center;">
+         </div>
+         <h3 style='text-transform: uppercase;'>${closestDayHeader.substring(0, 3)} ${closestAvailableDatesArray[i].getDate()}</h3>
+         <p >from</p>
+         <h3 >$250</h3>
+         <button class="homepage-booking-container-button" onclick="scrollToID('booking-auth-div')">BOOK</button>
+         </div>
+      `}
+      animationDuration += 200
+   }
+   closestAvailableDates += `
+   <div class="homepage-booking-container" onclick="scrollToID('booking-auth-div')">
+      BOOK NOW !
+   </div>`
+   availableDateDiv.innerHTML = closestAvailableDates
 }
-// renderCalendar()
+
+
+getClosestAvaliableDate()
+if (!user) renderAuth();
+else renderCalendar();
+
