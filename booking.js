ee752b8dce0458fcb9d0d3b3548d81bc08a8a102
K1mmyn <125098192+K1mmyn@users.noreply.github.com>
2023-08-25 13:56:55 +1200

    Update booking.js

--- booking.js
+++ booking.js
@@ -599,7 +599,7 @@ const getClosestAvaliableDate = () => {
       animationDuration += 200
    }
    closestAvailableDates += `
-   <div class="homepage-booking-container" onclick="scrollToID('booking-auth-div')" style="animation-duration: 2400ms;">
+   <div class="homepage-booking-container" onclick="scrollToID('booking-auth-div')">
       BOOK NOW !
    </div>`
    availableDateDiv.innerHTML = closestAvailableDates
